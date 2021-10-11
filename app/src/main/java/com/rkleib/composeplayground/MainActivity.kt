2f3f8e472a61414c2e929f004e942ef6aa1aba4c
Rommy K <rommy.kleib@gmail.com>
2021-10-11 12:06:05 +0700

    add profile picture

--- app/src/main/java/com/rkleib/composeplayground/MainActivity.kt
+++ app/src/main/java/com/rkleib/composeplayground/MainActivity.kt
@@ -3,10 +3,14 @@ package com.rkleib.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.rkleib.composeplayground.ui.theme.ComposePlaygroundTheme
 
@@ -14,25 +18,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposePlaygroundTheme {
-        Greeting("Android")
-    }
+fun PreviewMassageCard() {
+    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
 }
